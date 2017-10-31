06c9f81b5d3bd6a01ef09af348447b982b2e50e9
Megan <megan.e.dose@gmail.com>
2017-10-30 20:14:00 -0700

    Added notes

--- digit-root/digit-root.py
+++ digit-root/digit-root.py
@@ -9,3 +9,8 @@ def find_digit_root(num):
 nums = [20, 13, 4, 1111, 17]
 sorted_nums = sorted(nums)
 print sorted(sorted_nums, key=lambda num: find_digit_root(num))
+
+ # NOTE: when do complex sorting by more than one factor, is works backwards to
+ # how you would do it in English. Ex. Sort by regular nums first (to make sure
+ # 4 comes before 13;this is called stable sorting), THEN sort by the digit root
+ # using lambda (it would be the opposite via normal intution)
