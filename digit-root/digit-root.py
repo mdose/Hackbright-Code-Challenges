4cc93039090e4a416be5d979325bfbe3e7c0e850
Megan <megan.e.dose@gmail.com>
2017-10-30 20:09:33 -0700

    Part 2 of digit root sort problem (sort by num, then sort by digit root)

--- digit-root/digit-root.py
+++ digit-root/digit-root.py
@@ -4,4 +4,8 @@ def find_digit_root(num):
     else:
         return find_digit_root(sum([int(char) for char in str(num)]))
 
-print find_digit_root(123)
+# print find_digit_root(123)
+
+nums = [20, 13, 4, 1111, 17]
+sorted_nums = sorted(nums)
+print sorted(sorted_nums, key=lambda num: find_digit_root(num))
